7eb60348924ae0d0edf1ce012611401d230553dc
Pierced-End <adrianpplaza4@gmail.com>
2026-05-27 16:29:03 +0800

    Add files via upload

---
+++ sudoku-solver/src/com/sudoku/SudokuServlet.java
@@ -0,0 +1,92 @@
+package com.sudoku;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SudokuServlet extends HttpServlet {
+    private SudokuGenerator generator;
+    private Gson gson;
+    
+    @Override
+    public void init() {
+        generator = new SudokuGenerator();
+        gson = new Gson();
+        System.out.println("SudokuServlet initialized successfully!");
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+            throws IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter out = resp.getWriter();
+        
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "running");
+        response.put("algorithm", "Dancing Links (Algorithm X)");
+        out.print(gson.toJson(response));
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+            throws IOException {
+        String path = req.getPathInfo();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter out = resp.getWriter();
+        
+        try {
+            if ("/generate".equals(path)) {
+                int[][] puzzle = generator.generatePuzzle(30);
+                Map<String, Object> response = new HashMap<>();
+                response.put("puzzle", puzzle);
+                response.put("status", "success");
+                out.print(gson.toJson(response));
+                
+            } else if ("/solve".equals(path)) {
+                String body = req.getReader().lines().collect(Collectors.joining());
+                Map<String, Object> requestData = gson.fromJson(body, 
+                    new TypeToken<Map<String, Object>>(){}.getType());
+                
+                List<List<Double>> puzzleList = (List<List<Double>>) requestData.get("puzzle");
+                
+                int[][] puzzle = new int[9][9];
+                for (int i = 0; i < 9; i++) {
+                    List<Double> row = puzzleList.get(i);
+                    for (int j = 0; j < 9; j++) {
+                        puzzle[i][j] = row.get(j).intValue();
+                    }
+                }
+                
+                DancingLinksSolver solver = new DancingLinksSolver();
+                int[][] solution = new int[9][9];
+                for (int i = 0; i < 9; i++) {
+                    System.arraycopy(puzzle[i], 0, solution[i], 0, 9);
+                }
+                
+                boolean solved = solver.solve(solution);
+                List<List<Integer>> steps = solver.getSteps();
+                
+                Map<String, Object> response = new HashMap<>();
+                response.put("solved", solved);
+                response.put("solution", solution);
+                response.put("steps", steps);
+                response.put("status", "success");
+                
+                out.print(gson.toJson(response));
+            } else {
+                resp.setStatus(404);
+                out.print("{\"error\": \"Endpoint not found\"}");
+            }
+        } catch (Exception e) {
+            resp.setStatus(500);
+            Map<String, String> error = new HashMap<>();
+            error.put("error", e.getMessage());
+            out.print(gson.toJson(error));
+        }
+    }
+}
---
+++ sudoku-solver/src/main/java/com/sudoku/SudokuGenerator.java
@@ -0,0 +1,106 @@
+﻿package com.sudoku;
+
+import java.util.Random;
+
+public class SudokuGenerator {
+    private static final int SIZE = 9;
+    private Random random;
+    
+    public SudokuGenerator() {
+        this.random = new Random();
+    }
+    
+    public int[][] generatePuzzle(int clues) {
+        int[][] solution = generateFullGrid();
+        int[][] puzzle = new int[SIZE][SIZE];
+        
+        for (int i = 0; i < SIZE; i++) {
+            System.arraycopy(solution[i], 0, puzzle[i], 0, SIZE);
+        }
+        
+        int cellsToRemove = SIZE * SIZE - clues;
+        while (cellsToRemove > 0) {
+            int row = random.nextInt(SIZE);
+            int col = random.nextInt(SIZE);
+            if (puzzle[row][col] != 0) {
+                puzzle[row][col] = 0;
+                cellsToRemove--;
+            }
+        }
+        
+        return puzzle;
+    }
+    
+    private int[][] generateFullGrid() {
+        int[][] grid = new int[SIZE][SIZE];
+        fillGrid(grid);
+        return grid;
+    }
+    
+    private boolean fillGrid(int[][] grid) {
+        int[] cells = new int[SIZE * SIZE];
+        for (int i = 0; i < cells.length; i++) {
+            cells[i] = i;
+        }
+        shuffle(cells);
+        
+        for (int cell : cells) {
+            int row = cell / SIZE;
+            int col = cell % SIZE;
+            
+            if (grid[row][col] == 0) {
+                int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+                shuffle(numbers);
+                
+                for (int num : numbers) {
+                    if (isSafe(grid, row, col, num)) {
+                        grid[row][col] = num;
+                        if (isComplete(grid) || fillGrid(grid)) {
+                            return true;
+                        }
+                        grid[row][col] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+        return isComplete(grid);
+    }
+    
+    private boolean isSafe(int[][] grid, int row, int col, int num) {
+        for (int x = 0; x < SIZE; x++) {
+            if (grid[row][x] == num || grid[x][col] == num) {
+                return false;
+            }
+        }
+        
+        int boxRow = row - row % 3;
+        int boxCol = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (grid[boxRow + i][boxCol + j] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean isComplete(int[][] grid) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (grid[i][j] == 0) return false;
+            }
+        }
+        return true;
+    }
+    
+    private void shuffle(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
