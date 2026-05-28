cfd33392cbe466e658467acfa32f9308f0233aeb
Pierced-End <adrianpplaza4@gmail.com>
2026-05-28 13:21:20 +0800

    Add files via upload

---
+++ sudoku-solver/web/service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'sudoku-solver-v2';
+
+const urlsToCache = [
+    '/sudoku/',
+    '/sudoku/index.html',
+    '/sudoku/css/style.css',
+    '/sudoku/manifest.json',
+    '/sudoku/icons/icon-192x192.png',
+    '/sudoku/icons/icon-512x512.png'
+];
+
+// Install
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+            .then(() => self.skipWaiting())
+    );
+});
+
+// Fetch - Cache first, then network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) return response;
+                return fetch(event.request).then(response => {
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+                    const responseToCache = response.clone();
+                    caches.open(CACHE_NAME)
+                        .then(cache => cache.put(event.request, responseToCache));
+                    return response;
+                });
+            })
+    );
+});
+
+// Activate - Clean old caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
